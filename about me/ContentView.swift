63fc88737a883ec7c4ee2df20cf1e3f3cfe62ecc
Anissa Santos <anissa.n.santos@gmail.com>
2023-07-21 11:39:28 -0500

    Initial Commit

---
+++ about me/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  about me
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var titleText = "About me"
+    var body: some View {
+        
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
